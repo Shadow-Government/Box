c6c565d8055be72fe144cce0a2907ea2cad8224b
xGab0 <aricogabriele2001@gmail.com>
2023-05-28 17:48:45 +0200

    jitpack fix #4

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,11 @@ subprojects {
 
     repositories {
         mavenCentral()
+
+        maven {
+            name = "JitPack"
+            url  = uri("https://jitpack.io")
+        }
     }
 
     configure<JavaPluginExtension> {
@@ -17,6 +22,7 @@ subprojects {
 
         tasks.withType<JavaCompile> {
             options.encoding = "UTF-8"
+            options.compilerArgs.addAll(listOf("-Xlint:none", "-Xlint:-deprecation", "-Xlint:-unchecked"))
         }
 
         tasks.withType<Javadoc> {
@@ -26,12 +32,29 @@ subprojects {
 
     configure<PublishingExtension> {
         publications {
-            create<MavenPublication>("maven") {
+            create<MavenPublication>("mavenJava") {
                 groupId     = project.group as String?
                 artifactId  = project.name
                 version     = project.version as String?
 
                 from(components["java"])
+
+                pom {
+                    name.set(project.name)
+                    description.set("A concise description of my library")
+                    url.set("http://www.example.com/library")
+
+                    print(project.name)
+
+                    developers {
+                        developer {
+                            id.set("The-Crown-Studios")
+                            name.set("The Crown Studios")
+                            email.set("business@thecrownstudios.it")
+                            url.set("https://github.com/The-Crown-Studios")
+                        }
+                    }
+                }
             }
         }
     }
